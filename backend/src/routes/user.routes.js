ae7db72c16f5720713edcac979ceae22043b1591
Chirag-Zanpadiya <zanpadiyachirag123@gmail.com>
2025-06-04 18:02:13 +0530

    feat: add raw-body middleware for Clerk webhooks in user routes

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -9,7 +9,7 @@ import authUser from "../middlewares/auth.js";
 
 const userRouter = express.Router();
 
-userRouter.post("/webhooks", clerkWebhooks);
+userRouter.post("/webhooks", express.raw({ type: "*/*" }),  clerkWebhooks);
 
 // TODO: yaha pe basically jab user authenticated hoga tab bhi usko credits dikhane chahiye isliye middleware insert kiya authUser
 userRouter.get("/credits", authUser, userCredits);
