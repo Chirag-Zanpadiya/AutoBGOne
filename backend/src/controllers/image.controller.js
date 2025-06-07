add9809e64951904c9fd0d255dc9e5577052ae10
Chirag-Zanpadiya <zanpadiyachirag123@gmail.com>
2025-06-07 19:45:31 +0530

    fix: format response and logging in removeBGImage function

--- backend/src/controllers/image.controller.js
+++ backend/src/controllers/image.controller.js
@@ -8,13 +8,17 @@ import { User } from "../models/user.model.js";
 const removeBGImage = async (req, res) => {
   try {
     const { clerkId } = req.body;
+
+    console.log(`removeBGImage :: req.boyd :: ${req.body}`);
     console.log(`removeBGImage :: clerkId :: ${clerkId}`);
-    
 
     const user = await User.findOne({ clerkId });
 
     if (!user) {
-      return res.json({ success: false, message: "image.controller.js :: User Not Found" });
+      return res.json({
+        success: false,
+        message: "image.controller.js :: User Not Found",
+      });
     }
 
     if (user.creditBalance === 0) {
@@ -53,7 +57,12 @@ const removeBGImage = async (req, res) => {
       creditBalance: user.creditBalance - 1,
     });
 
-    res.json({ success: true, resultImage, creditBalance: user.creditBalance-1 , message : "BackGroun Removed"});
+    res.json({
+      success: true,
+      resultImage,
+      creditBalance: user.creditBalance - 1,
+      message: "BackGroun Removed",
+    });
   } catch (error) {
     console.log(
       `src :: controllers :: image.controller.js :: removeBGImage :: error :: ${error.message}`
